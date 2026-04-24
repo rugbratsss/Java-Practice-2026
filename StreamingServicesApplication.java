b206544be5ad3a3a432b4d8bd6046736f564871b
rugbratsss <rugbratsss@gmail.com>
2026-04-24 21:33:41 +0200

    Add launch configuration and initial setup for StreamingServicesApplication

---
+++ StreamingServicesApplication.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.Random;
+public class StreamingServicesApplication {
+    public static void populateSubscribers(int[] subscribers)
+    {
+        Random rand = new Random();
+
+        for(int i = 0; i < subscribers.length; i++){
+            subscribers[i] = r
+        }
+    }
+    public static void main(String[] args){
+        String[] serviceProviders = {"YouTube", "Netflix","DSTV", "ShowMax", "PrimeTV", "SABC"};
+        String[] originCountry = {"United States", "United States", "South Africa", "South Africa", "United States", "South Africa"}; 
+
+        int[] subscribers = new int (serviceProviders.length);
+        int[] userRatings = new int (serviceProviders.length);
+    }
+}
